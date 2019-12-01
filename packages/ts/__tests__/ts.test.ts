2c4c695cd60d7a85b8790619ec74311dd2781505
imeepos <1037483576@qq.com>
2019-12-01 17:56:41 +0800

    update tsconfig

--- packages/ts/__tests__/ts.test.ts
+++ packages/ts/__tests__/ts.test.ts
@@ -2,16 +2,25 @@ import { moduleRef } from '../lib';
 import * as ts from 'typescript';
 import { createProgram } from 'typescript';
 import { join } from 'path';
+const defaultCompilerOptions = ts.getDefaultCompilerOptions();
+const tsconfig = ts.readJsonConfigFile(join(__dirname, 'tsconfig.json'), (path: string) => {
+    return path;
+})
 const program = createProgram({
     rootNames: [
-        join(__dirname, 'src')
+        join(__dirname)
     ],
     options: {
+        ...defaultCompilerOptions,
         target: ts.ScriptTarget.ESNext,
         declaration: true
     }
 });
 const typeChecker = program.getTypeChecker();
-const sourceFiles = program.getSourceFiles();
-const files = sourceFiles.map(it => moduleRef.create(it, 'kind'))
+const fileNames = program.getSourceFiles().map(it => it.fileName)
+const sourceFile = program.getSourceFileByPath(join(__dirname, 'src', './demo.ts') as ts.Path);
+if (sourceFile) {
+    const files = moduleRef.create(sourceFile, 'kind')
+    debugger;
+}
 debugger;
