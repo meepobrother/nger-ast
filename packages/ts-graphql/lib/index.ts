adcca63ef7e474fbb8305cce0783df68690c4cc4
imeepos <1037483576@qq.com>
2019-12-06 10:09:28 +0800

    ts-graphql support tsconfig

--- packages/ts-graphql/lib/index.ts
+++ packages/ts-graphql/lib/index.ts
@@ -4,8 +4,20 @@ import { CompilerContext, TsGraphqlVisitor } from '@nger/ast.ts-graphql';
 import { Project } from '@nger/ast.tsc';
 import { DocumentNode } from '@nger/ast.graphql';
 import { NestDecoratorVisitor } from './handlers/decorator';
+import { join, dirname } from 'path';
+import { existsSync, readFileSync } from 'fs';
+import * as ts from 'typescript';
+const root = process.cwd();
+const tsconfigPath = getTsConfigPath(root);
+function getTsConfigPath(path: string): string {
+    const dist = join(path, 'tsconfig.json')
+    if (existsSync(dist)) {
+        return dist;
+    }
+    return getTsConfigPath(join(path, '..'))
+}
 export function tsGraphql(src: string): DocumentNode | undefined {
-    const project = new Project([src]);
+    const project = new Project([src], tsconfigPath);
     const compilerContext = new CompilerContext(project.program)
     const tsGraphqlVisitor = new TsGraphqlVisitor(new NestDecoratorVisitor());
     const node = project.getSourceFile(src);
--- packages/tsc/lib/project.ts
+++ packages/tsc/lib/project.ts
@@ -1,62 +1,23 @@
 import { moduleRef, SourceFile } from '../lib';
 import * as ts from 'typescript';
-import { createProgram } from 'typescript';
-import glob from 'glob';
+import { Project as TsMorphProject } from 'ts-morph';
 export class Project {
-    program: ts.Program;
-    typeChecker: ts.TypeChecker;
+    get program(): ts.Program {
+        return this._project.getProgram().compilerObject;
+    }
+    get typeChecker(): ts.TypeChecker {
+        return this._project.getTypeChecker().compilerObject
+    }
+    _project: TsMorphProject;
     constructor(
-        private rootNames: string[],
-        private compilerOptions?: ts.CompilerOptions
+        src: string[],
+        tsConfigFilePath: string
     ) {
-        this.program = createProgram({
-            rootNames: rootNames,
-            options: {
-                target: ts.ScriptTarget.ESNext,
-                declaration: true,
-                removeComments: false,
-                noEmit: false,
-                module: ts.ModuleKind.CommonJS,
-                ...compilerOptions || {}
-            }
+        this._project = new TsMorphProject({
+            tsConfigFilePath: tsConfigFilePath,
+            addFilesFromTsConfig: true
         });
-        this.typeChecker = this.program.getTypeChecker();
-    }
-    addDir(dir: string) {
-        return new Promise((resolve, reject) => {
-            glob(dir, (err: Error | null, files: string[]) => {
-                if (err) return reject(err);
-                this.rootNames.push(...files);
-                this.program = createProgram({
-                    rootNames: this.rootNames,
-                    options: {
-                        target: ts.ScriptTarget.ESNext,
-                        declaration: true,
-                        removeComments: false,
-                        noEmit: false,
-                        module: ts.ModuleKind.CommonJS,
-                        ...this.compilerOptions || {}
-                    }
-                });
-                this.typeChecker = this.program.getTypeChecker();
-                resolve(this)
-            })
-        })
-    }
-    addFile(file: string) {
-        this.rootNames.push(file);
-        this.program = createProgram({
-            rootNames: this.rootNames,
-            options: {
-                target: ts.ScriptTarget.ESNext,
-                declaration: true,
-                removeComments: false,
-                noEmit: false,
-                module: ts.ModuleKind.CommonJS,
-                ...this.compilerOptions || {}
-            }
-        });
-        this.typeChecker = this.program.getTypeChecker();
+        this._project.addSourceFilesAtPaths(src);
     }
     getSourceFile(file: string): SourceFile | undefined {
         const files = this.program.getSourceFiles();
