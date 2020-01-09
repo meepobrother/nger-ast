2bd4fa44fb5c1792d8338e9cad4b61fe641f176a
imeepos <admin@meepo.com.cn>
2020-01-09 18:13:15 +0800

    ast graphql

--- packages/tsc/__tests__/ts.test.ts
+++ packages/tsc/__tests__/ts.test.ts
@@ -1,8 +1,8 @@
 import { Project } from '../lib';
 import { join } from 'path';
 const project = new Project([
-    join(__dirname, 'src', './demo.ts')
-]);
+    join(__dirname, 'src', './demo.ts'),
+], join(__dirname, 'tsconfig.json'));
 const node = project.getSourceFile(join(__dirname, 'src', './demo.ts'))
 
 debugger;
